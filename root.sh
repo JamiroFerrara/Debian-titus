c5ed34a493255af89ba79fc81f815f25565c39dc
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-06-04 14:55:22 +0200

    commit

--- root.sh
+++ root.sh
@@ -3,6 +3,7 @@
 # Change Debian to SID Branch
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp sources.list /etc/apt/sources.list 
+cp ./bashrc /home/$username/
 
 username=$(id -u -n 1000)
 builddir=$(pwd)
@@ -40,7 +41,6 @@ chown -R $username:$username /home/$username
 
 source $builddir/installbrave.sh
 
-#source $builddir/installnvim.sh
 #install nvim
 sudo apt-get install -y neovim
 mkdir /home/stiwie/Downloads
@@ -49,16 +49,11 @@ wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.tar.
 tar xzvf nvim-linux64.tar.gz
 mv /home/stiwie/Downloads/nvim-linux64/bin/nvim /usr/bin
 
-#source $builddir/installnode.sh
 apt install -y nodejs
 apt-get install -y npm
 
-#STAGE 2
 npm install --location=global yarn
 
-#source ./installspotify.sh
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 apt-get update && sudo apt-get install -y spotify-client
-
-#source $builddir/chezmoi.sh
