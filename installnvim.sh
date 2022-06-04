5d64182c3a08adc7ce736cbe234ac9352c62d119
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-06-04 11:33:12 +0200

    commit

--- installnvim.sh
+++ installnvim.sh
@@ -9,5 +9,5 @@ tar xzvf nvim-linux64.tar.gz
 mv /home/stiwie/Downloads/nvim-linux64/bin/nvim /usr/bin
 rm -dfr /home/stiwie/
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+bash -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
---
+++ root2.sh
@@ -0,0 +1,2 @@
+chezmoi init https://github.com/jamiroferrara/dotfiles
+chezmoi apply --force -v
